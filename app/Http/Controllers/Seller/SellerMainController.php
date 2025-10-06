b7dd949b6113aa4460808938950a14d1e31ef7b6
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-10-06 10:49:05 +0300

    Add Seller controllers for main, product, and store management with placeholder methods and views for improved structure and organization

---
+++ app/Http/Controllers/Seller/SellerMainController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Seller;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SellerMainController extends Controller
+{
+    public function index()
+    {
+        return view('seller.seller');
+
+    }
+    public function order_history()
+    {
+        return view('seller.order.history');
+    }
+    public function settings()
+    {
+        return view('seller.settings');
+    }
+}
