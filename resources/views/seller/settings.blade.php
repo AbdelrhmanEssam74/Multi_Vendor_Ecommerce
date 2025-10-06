2d7a9484f0c4b240db6efa13505205b0cd8f430e
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-10-06 10:48:26 +0300

    Replace placeholder seller Blade template with extended layout and add new settings view for improved organization and consistency.

---
+++ resources/views/seller/settings.blade.php
@@ -0,0 +1,8 @@
+@extends('seller.layouts.layout')
+@section('title', 'Seller | Settings ')
+@section('description')
+    Seller  Settings
+@endsection
+@section('seller_layout')
+    <h1> Settings </h1>
+@endsection
