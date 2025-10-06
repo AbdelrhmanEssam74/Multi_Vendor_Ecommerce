a255d7a99c8a003995563f06b2b035ec403ea937
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-10-07 02:06:25 +0300

    Add structured customer views with extended layout templates for profile, order history, payment settings, and affiliate pages to improve consistency and organization.

---
+++ resources/views/customer/payment/settings.blade.php
@@ -0,0 +1,8 @@
+@extends('customer.layouts.layout')
+@section('title', 'Payment Settings')
+@section('description')
+    Payment Settings
+@endsection
+@section('customer_layout')
+    <h1> Payment Settings</h1>
+@endsection
---
+++ resources/views/customer/profile.blade.php
@@ -0,0 +1,8 @@
+@extends('customer.layouts.layout')
+@section('title', 'Profile')
+@section('description')
+    This is the profile page where users can view and edit their personal information.
+@endsection
+@section('customer_layout')
+    <h1>customer profile</h1>
+@endsection
