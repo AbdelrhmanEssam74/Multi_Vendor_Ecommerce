67695d6a08fdd4b37c5f661aa35c7e31df175790
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-10-17 19:28:36 +0300

    Add Seller and Store models with relationships to User and Category

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,7 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function seller(){
+        return $this->hasOne(Seller::class, 'user_id');
+    }
 }
