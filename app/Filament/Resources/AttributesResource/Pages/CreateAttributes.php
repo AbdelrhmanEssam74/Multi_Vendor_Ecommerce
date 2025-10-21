77e5ad102123cf19ebd4c7a0edf3a2059df23a33
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-10-21 08:15:48 +0300

    Add Filament-based `AttributesResource` for attribute management, including CRUD operations and UI customization

---
+++ app/Filament/Resources/AttributesResource/Pages/CreateAttributes.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Filament\Resources\AttributesResource\Pages;
+
+use App\Filament\Resources\AttributesResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateAttributes extends CreateRecord
+{
+    protected static string $resource = AttributesResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return self::getResource()::getUrl('index');
+    }
+}
