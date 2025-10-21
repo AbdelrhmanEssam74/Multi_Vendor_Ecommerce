729b92b6c99d2085525d9f3a167e46c9977a92c0
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-10-21 23:47:36 +0300

    Add Filament-based `AttributesResource` for attribute management, including CRUD operations and UI customization

---
+++ app/Filament/Seller/Resources/StoreResource/Pages/CreateStore.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Filament\Seller\Resources\StoreResource\Pages;
+
+use App\Filament\Seller\Resources\StoreResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateStore extends CreateRecord
+{
+    protected static string $resource = StoreResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+
+        $data['seller_id'] = auth()->user()->seller->seller_id;
+
+        return $data;
+    }
+}
